558678b7e94885d0485ae205ac803420a470b407
MIE-CODE <israelvictor126@gmail.com>
2025-08-28 17:31:46 +0100

    added more seo

--- src/app/Providers/Provider.tsx
+++ src/app/Providers/Provider.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
 import SEO from "../../../next-seo.config";
 export const Provider = (props: PropsWithChildren<unknown>) => {
+  if (typeof window === "undefined") {
+    return <>{props.children}</>;
+  }
   return (
     <ThemeProvider>
       <DefaultSeo {...SEO} />
